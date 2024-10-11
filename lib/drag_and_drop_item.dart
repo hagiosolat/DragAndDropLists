e7c4e2bde7ba009bc5f85059b1e7662fd0952a41
Apratim Arora <41536312+apratim-arora@users.noreply.github.com>
2024-10-11 17:29:35 +0530

    Update drag_and_drop_item.dart
    added import references and data members to DragAndDropItem class to support this package's functionalities as well as that of 'flutter_fancy_tree_view' TreeNode.

--- lib/drag_and_drop_item.dart
+++ lib/drag_and_drop_item.dart
@@ -1,5 +1,7 @@
 import 'package:drag_and_drop_lists/drag_and_drop_interface.dart';
 import 'package:flutter/widgets.dart';
+import 'package:responsive_1/src/features/folder_view/domain/folder_view_models.dart'; //addition
+import 'package:responsive_1/src/features/articles/domain/article_models.dart'; // additional import referencing Article & Priority classes in my project.
 
 class DragAndDropItem implements DragAndDropInterface {
   /// The child widget of this item.
@@ -13,10 +15,29 @@ class DragAndDropItem implements DragAndDropInterface {
   /// Set to false if it must remain fixed.
   final bool canDrag;
   final Key? key;
+  //additions
+  bool get isFolder => article == null;
+  final String title;
+  final Article? article;
+  final List<DragAndDropItem> children;
+  DragAndDropItem? parent;
+
   DragAndDropItem({
     required this.child,
     this.feedbackWidget,
     this.canDrag = true,
     this.key,
-  });
+    //additions
+    required this.title,
+    Iterable<DragAndDropItem>? childList,
+    this.article,
+  }) : children = <DragAndDropItem>[] {
+    if (childList == null) return;
+
+    for (final DragAndDropItem child in childList) {
+      children.add(child);
+      child.parent = this;
+    }
+  }
 }
+
