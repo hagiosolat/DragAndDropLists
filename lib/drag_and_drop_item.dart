b865fe15a6d0ad9dd3d38f51139e8f243216d68d
Apratim Arora <41536312+apratim-arora@users.noreply.github.com>
2024-10-14 23:34:55 +0530

    Update drag_and_drop_item.dart
    made child parameter not compulsory to be provided at the time of object creation and made default feedbackWidget.

--- lib/drag_and_drop_item.dart
+++ lib/drag_and_drop_item.dart
@@ -2,13 +2,16 @@ import 'package:drag_and_drop_lists/drag_and_drop_interface.dart';
 import 'package:flutter/widgets.dart';
 import 'package:responsive_1/src/features/folder_view/domain/folder_view_models.dart'; //addition
 import 'package:responsive_1/src/features/articles/domain/article_models.dart'; // additional import referencing Article & Priority classes in my project.
+import 'package:flutter/material.dart'; //additional
+import 'package:flutter/src/foundation/constants.dart'; //additional
+import 'package:responsive_1/src/features/priority_view/presentation/widgets.dart'; //additional
 
 class DragAndDropItem implements DragAndDropInterface {
   /// The child widget of this item.
-  final Widget child;
+  Widget child;
 
   /// Widget when draggable
-  final Widget? feedbackWidget;
+  Widget? feedbackWidget;
 
   /// Whether or not this item can be dragged.
   /// Set to true if it can be reordered.
@@ -23,15 +26,25 @@ class DragAndDropItem implements DragAndDropInterface {
   DragAndDropItem? parent;
 
   DragAndDropItem({
-    required this.child,
     this.feedbackWidget,
     this.canDrag = true,
     this.key,
+    this.child = const SizedBox(),
     //additions
     required this.title,
     Iterable<DragAndDropItem>? childList,
     this.article,
   }) : children = <DragAndDropItem>[] {
+    if (feedbackWidget == null) {
+      var fb = MyFeedbackWidget(title);
+      feedbackWidget =
+          // kIsWeb ?
+          Transform.translate(offset: const Offset(-10, -16), child: fb);
+      // : Transform.translate(
+      //     offset: const Offset(-50, -50),
+      //     child: fb,
+      //   );
+    }
     if (childList == null) return;
 
     for (final DragAndDropItem child in childList) {
@@ -40,4 +53,3 @@ class DragAndDropItem implements DragAndDropInterface {
     }
   }
 }
-
