c52dd4842395f9d4cc24cd35e25ba1f7fdeccb51
Apratim Arora <41536312+apratim-arora@users.noreply.github.com>
2025-01-27 18:13:56 +0530

    assign child in constructor

--- lib/drag_and_drop_item.dart
+++ lib/drag_and_drop_item.dart
@@ -1,12 +1,12 @@
 import 'package:drag_and_drop_lists/drag_and_drop_interface.dart';
+import 'package:flutter/material.dart'; //additional
 import 'package:flutter/widgets.dart';
 import 'package:responsive_1/src/features/articles/domain/article_models.dart'; // additional import referencing Article & Priority classes in my project.
-import 'package:flutter/material.dart'; //additional
 import 'package:responsive_1/src/features/priority_view/presentation/widgets.dart'; //additional
 
 class DragAndDropItem implements DragAndDropInterface {
   /// The child widget of this item.
-  Widget child;
+ Widget child;
 
   /// Widget when draggable
   Widget? feedbackWidget;
@@ -19,7 +19,7 @@ class DragAndDropItem implements DragAndDropInterface {
   //additions
   bool get isFolder => article == null;
   final String title;
-  final Article? article;
+  Article? article;
   final List<DragAndDropItem> children;
   DragAndDropItem? parent;
 
@@ -33,6 +33,11 @@ class DragAndDropItem implements DragAndDropInterface {
     Iterable<DragAndDropItem>? childList,
     this.article,
   }) : children = <DragAndDropItem>[] {
+    if (isFolder) {
+      child = MyFeedbackWidget(title);
+    } else {
+      child = DragAndDropContainer(article!);
+    }
     if (feedbackWidget == null) {
       var fb = MyFeedbackWidget(title);
       feedbackWidget =
@@ -44,5 +49,24 @@ class DragAndDropItem implements DragAndDropInterface {
       children.add(child);
       child.parent = this;
     }
+  }
+
+  DragAndDropItem copyWith({
+    Widget? child,
+    Widget? feedbackWidget,
+    bool? canDrag,
+    Key? key,
+    String? title,
+    Article? article,
+    List<DragAndDropItem>? children,
+  }) {
+    return DragAndDropItem(
+      child: child ?? this.child,
+      feedbackWidget: feedbackWidget ?? this.feedbackWidget,
+      canDrag: canDrag ?? this.canDrag,
+      key: key ?? this.key,
+      title: title ?? this.title,
+      article: article ?? this.article,
+    );
   }
 }
